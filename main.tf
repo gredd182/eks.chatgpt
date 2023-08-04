e6cd7155e5ed3f94727666e1e57b49ef07f8d96b
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-04 08:17:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,79 +48,14 @@ resource "aws_iam_role" "eks_cluster" {
   })
 }
 
-resource "aws_launch_configuration" "example" {
-  name                 = "eks-launch-config"
-  image_id             = var.instance_ami
-  instance_type        = var.instance_type
-  security_groups      = [aws_security_group.eks_cluster.id]
-  iam_instance_profile = aws_iam_instance_profile.example.arn
-  key_name             = var.ssh_key_name
+module "eks_node_group" {
+  source = "terraform-aws-modules/eks/aws//modules/managed_node_group"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  name                 = "eks-asg"
-  launch_configuration = aws_launch_configuration.example.id
-  min_size             = var.min_nodes
-  max_size             = var.max_nodes
-  desired_capacity     = var.desired_capacity
-  vpc_zone_identifier  = aws_subnet.public[*].id
-  tags = [
-    {
-      key                 = "Name"
-      value               = "eks-node"
-      propagate_at_launch = true
-    },
-  ]
-}
-
-resource "aws_security_group" "eks_cluster" {
-  name_prefix = "eks-cluster-sg-"
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_iam_instance_profile" "example" {
-  name = "eks-instance-profile"
-
-  role = aws_iam_role.eks_instance_profile.name
-}
-
-resource "aws_iam_role" "eks_instance_profile" {
-  name = "eks-instance-profile-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
+  cluster_name    = aws_eks_cluster.example.name
+  subnets         = aws_subnet.public[*].id
+  instance_type   = var.instance_type
+  desired_capacity = var.desired_capacity
+  tags            = var.tags
 }
 
 output "kubeconfig" {
